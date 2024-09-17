46631d7165726afccd4ef2a43a709c2ee132bd54
Chris Schooley <chris@schooley.name>
2024-09-17 14:37:33 -0600

    make call to binary_search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-mod linearsearch;
+mod linear_search;
+mod binary_search;
 
 fn main() {
     let arr: [i32; 5] = [3, 5, 7, 9, 11]; 
 
-    let r = linearsearch::linearsearch( &arr, 5);
+    let r = linear_search::linear_search( &arr, 5);
     println!("{r}");
+
+    let r = binary_search::binary_search(&arr, 5);
+    println!("{r}");
+
+
+
 }
