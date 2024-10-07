630dc70610ece402665d8b80bc1f565d9ea87951
Chris Schooley <chris@schooley.name>
2024-10-07 17:44:05 -0600

    binary search stub

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,11 +1,12 @@
 use std::usize;
 
-pub fn binary_search (arr: &[i32], x: i32) -> usize {
+pub fn binary_search (arr: &[i32], x: i32) -> i32 {
     let mut low: i32 = 0;
     let mut high = arr.len().try_into().unwrap();
-    
+    high = high-1;
+
     while low <= high {
-        let mid = (low + high) % 2;
+        let mid = (low + high) / 2;
         if arr[mid as usize] > x {
             low = mid + 1;
         }
